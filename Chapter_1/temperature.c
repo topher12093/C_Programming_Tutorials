d59a8618467ce8ea6a1dac789577e9b9f97cfec5
topher12093 <deluciac@bu.edu>
2017-03-08 11:15:27 -0500

    update temperature

--- Chapter_1/temperature.c
+++ Chapter_1/temperature.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 
-main()
+
+//Function Prototype
+int convert(int, int);
+
+//Main
+int main()
 {
 	float fahr, celsius;
 	int lower, upper, step;
@@ -8,13 +13,21 @@ main()
 	lower = 0;  //Lower bound
 	upper = 300; //Upper bound
 	step = 20; //Step amount
-
-	fahr = upper;
 	
-	printf("Fahr\tCelsius\n\n "); 
+	printf("Fahr\tCelsius\n\n ");
 
 	for(fahr = upper; fahr >= lower; (fahr = (fahr - step))){
-		celsius = 5 * (fahr-32) / 9;
+		celsius = convert(celsius, fahr);
 		printf("%6.2f\t%6.2f\n", fahr, celsius);
 	}
+	
+	return 0;
+}
+
+//Convert function
+int convert(int celsius, int fahr){	
+
+	celsius = 5 * (fahr-32) / 9;
+	
+	return celsius;
 }
